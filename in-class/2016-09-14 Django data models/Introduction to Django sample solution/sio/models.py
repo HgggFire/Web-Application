3f0855d067827ae96c1a2bbb342f0abb328fe5b4
Chico Lin <soap.cmu@gmail.com>
2016-09-14 17:15:46 -0400

    09/14 inclass exercise submission

--- in-class/2016-09-14 Django data models/Introduction to Django sample solution/sio/models.py
+++ in-class/2016-09-14 Django data models/Introduction to Django sample solution/sio/models.py
@@ -3,3 +3,24 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    andrewId = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.andrewId
+
+    class Meta:
+        ordering = ('andrewId',)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    id = models.IntegerField()
+    students = models.ManyToManyField(Student)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('id',)
