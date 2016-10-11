9116abdf969c67938a7a1c87cba9557316245c16
Chico Lin <soap.cmu@gmail.com>
2016-10-11 11:31:15 -0400

    finished in-class exercice 10/10

--- in-class/2016-10-10 Ajax/fortune.js
+++ in-class/2016-10-10 Ajax/fortune.js
@@ -1,5 +1,24 @@
-
+var req;
+var count = 0;
 function getFortune() {
-
+    // count++;
+    // var p = document.getElementById("info");
+    // p.innerHTML = count;//req.readyState;
+    sendRequest();
+}
+// Sends a new request to update the to-do list
+function sendRequest() {
+    if (window.XMLHttpRequest) {
+        req = new XMLHttpRequest();
+    } else {
+        req = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    req.onreadystatechange = handleResponse;
+    req.open("GET", "http://garrod.isri.cmu.edu/webapps/fortune", true);
+    req.send(); 
 }
 
+function handleResponse() {
+    var p = document.getElementById("content");
+    p.innerHTML = req.responseText;
+}
